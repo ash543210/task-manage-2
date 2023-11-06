a8200dfa3733188bc6825779785f8252db8c033f
ash543210 <shaikashraf54321@gmail.com>
2023-11-06 13:31:04 +0530

    corsoptions

--- functions/api.js
+++ functions/api.js
@@ -4,9 +4,15 @@ const serverless = require("serverless-http");
 const mongoose = require("mongoose");
 const tasksRoutes = require("../src/routes/task.js");
 const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 // const func = async () => {
 //   const result = await fetch(
@@ -21,7 +27,7 @@ app.use(express.json());
 
 mongoose
   .connect(
-    "mongodb+srv://ash54321:taGpWfeSyhuXfFgW@cluster0.si5umyq.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://ash54321:NldFgtHh8AYqti5D@cluster0.si5umyq.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected!"));
 
